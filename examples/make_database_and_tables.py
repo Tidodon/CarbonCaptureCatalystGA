55164f47951ce680d7fbd9dfdf708455c518be88
Dominik B. Orlowski <bnw543@alumni.ku.dk>
2023-10-23 13:28:15 +0200

    Database talking is now functionalized. It allows for table deletion, creation, and reading.

--- examples/make_database_and_tables.py
+++ examples/make_database_and_tables.py
@@ -5,10 +5,6 @@ import sqlite3
 conn = sqlite3.connect('/groups/kemi/orlowski/CarbonCapture/CarbonCaptureCatalystGA/examples/molecules_data.db')
 c = conn.cursor()
 
-
-
-
-
 def build_database(c):
    c.execute("""CREATE TABLE reactants(
         id INTEGER PRIMARY KEY,
@@ -41,42 +37,35 @@ def build_database(c):
    c.execute("ALTER TABLE reactants ADD comments text")
    c.execute("ALTER TABLE products ADD comments text")
    
-#build_database(c)
 
+def empty_dbs(cursor, *args):
+   ##Possible args in molecules_data.db: reactants, miscs, products.
+   for arg in args:
+      try: 
+         query = f"DELETE FROM {arg}"
+         cursor.execute(query)
+      except:
+         print(f"Table with name {arg} does not exist.")
+         
+def print_table_contents(cursor, *args):
+   for arg in args:
+      try: 
+         query= f"SELECT * FROM {arg}"
+         cursor.execute(query)
+         print(f"{arg} column names: ", [desc[0] for desc in cursor.description])
+         v = cursor.fetchall()
+         for val in v:
+            print(val)
+      except:
+         print(f"Table {arg} does not exist.")
 
-# # many_reactants = [ ('O','gfn2','gbsa','-5.080122224999'),
-# #                   ('O=C=O','gfn2','gbsa','-10.306805408736'),
-# #                   ('O','gfn2','alpb','-5.085033168595'),
-# #                   ('O=C=O','gfn2','alpb','-10.305467633322'),
-# #                   ('O','gfn1','alpb','-5.790291412071'),
-# #                   ('O=C=O','gfn1','alpb','-11.543823125467')]
-
-# # c.executemany("INSERT INTO miscs(smiles, method, solvation, energy) VALUES(?,?,?,?)", many_reactants)
-
-# products = [('O=C([O-])O','gfn2','gbsa','-15.215593476193'),
-#             ('O=C([O-])O','gfn2','alpb','-15.218909360801'),
-#             ('O=C([O-])O','gfn1','alpb','-17.131104157996'),
-#             ]
-# # c.executemany("INSERT INTO miscs(smiles, method, solvation, energy) VALUES(?,?,?,?)", products)
-
-# c.execute("""UPDATE reactants SET product_1_id=products.id
-#         FROM products
-#         WHERE reactants.method=products.method 
-#         AND reactants.solvation=products.solvation""")
 
 ###Code to get column names:
-c.execute("SELECT * FROM reactants")
-v = c.fetchall()
-for val in v:
-    print(val)
-print("Reactant column names: ", [desc[0] for desc in c.description])
-
-def empty_dbs(connector):
-   connector.execute("DELETE FROM products")
-   connector.execute("DELETE FROM reactants")
-   connector.execute("DELETE FROM miscs")
-
+#build_database(c, name1,name2)
 # empty_dbs(c)
+
+
+
 
 c.execute("SELECT * FROM miscs")
 print("Miscs column names: ", [desc[0] for desc in c.description])
